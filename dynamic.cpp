097c789e97245172c5cf485d67074beb7e92de10
DLopez200 <ld30930@mail.harpercollege.edu>
2026-06-14 19:06:30 -0500

    memory leaks

--- dynamic.cpp
+++ dynamic.cpp
@@ -6,6 +6,8 @@ int main(){
 
     //new int; //"new" can be used to request memory//
     int* ptr = new int; //ptr is on the stack that points to int on heap
+    *ptr = 100;
+
 
     cout << ptr << endl;
     cout << *ptr << endl;
