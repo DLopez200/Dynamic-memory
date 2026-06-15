b4da837ad2fa522558a0e56534e0eb557295124e
DLopez200 <ld30930@mail.harpercollege.edu>
2026-06-14 19:45:18 -0500

    handling new arrays
    creating arrays with new command and how to delete them after

--- dynamic.cpp
+++ dynamic.cpp
@@ -26,9 +26,28 @@ int main(){
     {
     number n = number(123);
     n.print();
+    }//bracket just create a local loop allowing the number to be destroyed once 
+     //it not needed rather than when the program ends
+    
+    
+    //we can allocate space for more than just int//
+    char* c= new char;
+    string* s = new string;
+
+    delete c;
+    delete s;
+
+    int* arr = new int [10];// this crates a array of 10 at start but it could be any size at start up
+    arr[0] = 123;
+    arr[1] = 123123;
+    arr[9] = 8324;
+
+    for(int i=0; i <10; i++){
+        cout << arr[i] <<endl;
     }
-    
-    
+    delete[] arr; // the special command to delete the hole array//
+    //other whise: "delete arr" whould just delete the variable it points to//
+
     //new int; //"new" can be used to request memory//
     int* ptr = new int; //ptr is on the stack that points to int on heap
     *ptr = 100;
